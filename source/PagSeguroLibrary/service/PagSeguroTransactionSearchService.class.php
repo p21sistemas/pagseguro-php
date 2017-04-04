d5a830f61efd83723633967f7bc380533ed78636
Alfredo Costa <alfredocosta@live.com>
2017-04-04 14:51:59 -0300

    Update PagSeguroTransactionSearchService.class.php

--- source/PagSeguroLibrary/service/PagSeguroTransactionSearchService.class.php
+++ source/PagSeguroLibrary/service/PagSeguroTransactionSearchService.class.php
@@ -373,7 +373,7 @@ class PagSeguroTransactionSearchService
      * @return bool|PagSeguroTransactionSearchResult
      * @throws PagSeguroServiceException
      */
-    private function searchResult($connection, $initialDate = null, $finalDate = null)
+    private static function searchResult($connection, $initialDate = null, $finalDate = null)
     {
 
         $httpStatus = new PagSeguroHttpStatus($connection->getStatus());
